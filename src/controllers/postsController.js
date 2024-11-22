63908306ff02990c735b0dbf1bdbafbe02fef2f1
Richard Ferreira <richard.ferreira.1@gmail.com>
2024-11-21 21:24:09 -0300

    aula 4

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,4 +1,5 @@
-import getTodosPosts from "../models/postModel.js";
+import {getTodosPosts, criarPost} from "../models/postModel.js";
+import fs from "fs"
 
 export async function listarPosts(req, res) {
     // Chama a função para buscar os posts e envia a resposta para o cliente.
@@ -6,3 +7,30 @@ export async function listarPosts(req, res) {
     res.status(200).json(posts);
 }
  
+export async function postarNovoPost(req, res) {
+    const novoPost = req.body;
+    try {
+        const postCriado = await criarPost(novoPost)
+        res.status(200).json(postCriado);
+    } catch {
+        console.error(erro.message);
+        res.status(500).json({"Erro":"Falha na resquisição"})
+    }
+}
+
+export async function uploadImagem(req, res) {
+    const novoPost = {
+        descricao:"",
+        imgurl: req.file.originalname,
+        alt: ""
+    };
+    try {
+        const postCriado = await criarPost(novoPost);
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`
+        fs.renameSync(req.file.path, imagemAtualizada)
+        res.status(200).json(postCriado);
+    } catch {
+        console.error(erro.message);
+        res.status(500).json({"Erro":"Falha na resquisição"})
+    }
+}
--- src/models/postModel.js
+++ src/models/postModel.js
@@ -4,7 +4,7 @@ import conectarAoBanco from "../config/dbConfig.js"
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO)
 
 // Função assíncrona para buscar todos os posts do banco de dados.
-export default async function getTodosPosts(){
+export async function getTodosPosts(){
     // Seleciona o banco de dados 'imersao-alura' e a coleção 'posts'.
     const db = conexao.db('imersao-alura')
     const colecao = db.collection ('posts')
@@ -12,3 +12,8 @@ export default async function getTodosPosts(){
     return colecao.find().toArray()
   }
   
+  export async function criarPost(novoPost) {
+    const db = conexao.db('imersao-alura')
+    const colecao = db.collection ('posts')
+    return colecao.insertOne(novoPost)
+  }
