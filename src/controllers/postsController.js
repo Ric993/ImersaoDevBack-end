d091761c54f90f3c77907865b177eb52f03cfdf1
Richard Ferreira <richard.ferreira.1@gmail.com>
2024-11-22 20:04:49 -0300

    att post

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,4 +1,4 @@
-import {getTodosPosts, criarPost} from "../models/postModel.js";
+import {getTodosPosts, criarPost, atualizarPost} from "../models/postModel.js";
 import fs from "fs"
 
 export async function listarPosts(req, res) {
@@ -28,6 +28,23 @@ export async function uploadImagem(req, res) {
         const postCriado = await criarPost(novoPost);
         const imagemAtualizada = `uploads/${postCriado.insertedId}.png`
         fs.renameSync(req.file.path, imagemAtualizada)
+        res.status(200).json(postCriado);
+    } catch {
+        console.error(erro.message);
+        res.status(500).json({"Erro":"Falha na resquisição"})
+    }
+}
+
+export async function atualizarNovoPost(req, res) {
+    const id = req.params.id;
+    const urlImagem = `http:/localhost:3000/${id}.png`
+    const post = {
+        imgurl: urlImagem,
+        descricao: req.body.descricao,
+        alt: req.body.alt
+    }
+    try {
+        const postCriado = await atualizarPost(id, Post)
         res.status(200).json(postCriado);
     } catch {
         console.error(erro.message);
